1d2be0683a76f211373fb93afa874015c990d80e
KathyA <100660595+kitkat-creat@users.noreply.github.com>
2022-07-04 20:30:38 -0700

    Add files via upload

---
+++ roman.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class romanType
+{
+public:
+	
+	
+	
+int convertRomantoDecimal(string romanvalue);
+private:
+	
+	string romanvalue;
+	
+	
+	
+};
+
